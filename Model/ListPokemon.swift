3cf0104bdfe18543004ef95f4f2835c5e1f52d88
MAC19 <mac19@MacBook-Air-de-MAC19.local>
2021-11-30 11:10:36 -0500

    primer commit

---
+++ Model/ListPokemon.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct ListPokemon: Codable {
+    let name: String
+    let url: String
+}
+
---
+++ View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeApi
+//
+//  Created by MAC19 on 30/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
